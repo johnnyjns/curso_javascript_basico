72e0445686af813fc69c3e29a090b7d5c979a07d
Johnny Souto <johnnyjns@gmail.com>
2016-12-21 21:48:18 -0200

    Alteração na altura do Tomás

--- projeto/percorre.js
+++ projeto/percorre.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function percorreArray(trsPacientes, comportamento){
 
     for (var posicaoAtual = 0; posicaoAtual <= trsPacientes.length -1; posicaoAtual++) {
@@ -5,4 +6,11 @@ function percorreArray(trsPacientes, comportamento){
         var pacienteTrAtual = trsPacientes[posicaoAtual];
         comportamento(pacienteTrAtual);
     }
+=======
+function percorreArray(trsPacientes){
+
+  for (var posicaoAtual = 0; posicaoAtual <= trsPacientes.length -1; posicaoAtual++) {
+
+  }
+>>>>>>> Stashed changes
 }
